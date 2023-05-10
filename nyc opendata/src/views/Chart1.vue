0503ccfd9998be2c093a8940a8a98c5db462d531
kaitlyndeng <kaitlyndeng20@gmail.com>
2023-05-10 10:10:44 -0400

    Commit 2

---
+++ nyc opendata/src/views/Chart1.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="doughnut">
+    <h1>Data in Doughnut.</h1>
+  </div>
+</template>
+<script>
+import { ref, onMounted } from 'vue'
+</script>
+<style></style>
--- nyc opendata/src/views/HomeView.vue
+++ nyc opendata/src/views/HomeView.vue
@@ -1,9 +1,18 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+<template>
+  <div></div>
+</template>
+
+<script>
+import { ref, onMounted } from 'vue'
+const math = ref('')
+async function getData() {
+  let res = await fetch('https://data.cityofnewyork.us/resource/m27t-ht3h.json')
+  let data = await res.json()
+  math.value = data.results
+}
+onMounted(() => {
+  getData()
+})
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style scoped></style>
